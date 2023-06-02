e8ac463210c62ae09af48a2076f7456403d0041e
mdzaman23 <119610605+mdzaman23@users.noreply.github.com>
2023-06-03 00:42:28 +0600

    Add files via upload

---
+++ botstrap-slider-porfolio-6/scrept.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.typing', {
+    strings: ['...Youtuber', 'Freelancer', 'Blogger', 'Desginer', 'Web Developer','Front-End Developer'],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+    
+  });
